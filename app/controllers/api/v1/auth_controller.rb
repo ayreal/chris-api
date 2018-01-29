77eca73bf0ea7cbd13038083dc5f263e0b3ae2f0
Ariel Herman <aeh.herman@gmail.com>
2018-01-28 20:03:28 -0500

    complete show route to retrieve current authed user

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::AuthController < ApplicationController
     user = User.find_by(name: params[:auth][:name])
     if user && user.authenticate(params[:auth][:password])
        # issue user a token
-      payload = {userId: user.id}
+      payload = {user_id: user.id}
       token = JWT.encode(payload, ENV["MY_SECRET"], ENV["ALG"])
       render json: {
         token: token
@@ -16,14 +16,13 @@ class Api::V1::AuthController < ApplicationController
   end
 
   def show
-    byebug
     token = params[:token]
     decoded_token =  JWT.decode(token, ENV['MY_SECRET'], ENV['ALG'] )
     # => [{"user_id"=>23}, {"alg"=>"HS256"}]
     user_id = decoded_token.first['user_id']
     user = User.find_by(id: user_id)
     if user
-      render json: user.package_json
+      render json: user.lists
     else
       render json: {error: "Could not find this user. Username or password is incorrect."}, status: 401
     end
