be66f0e91ca9db49dae330a7e5800bc8381fb686
Ariel Herman <aeh.herman@gmail.com>
2018-01-28 20:56:05 -0500

    complete create user route

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,16 @@
 class Api::V1::UsersController < ApplicationController
   def create
-    byebug
+    user = User.new(name: params[:newName])
+    user.password = params[:newPassword]
+    if user.valid?
+      user.save!
+      payload = {user_id: user.id}
+      token = JWT.encode(payload, ENV["MY_SECRET"], ENV["ALG"])
+      render json: {
+        token: token
+       }
+    else
+      render json: {error: "Could not authorize this user"}, status: 401
+    end
+   end
   end
-end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_many :lists
-  has_secure_password
+  has_secure_password validations: false
+  validates :name, presence: true
+  validates :name, uniqueness: true
 end
