d68ddb7f82db8d5c7674c006d3dbf6d075bee5fd
Ariel Herman <aeh.herman@gmail.com>
2018-01-30 19:53:57 -0500

    add items#create logic

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,2 +1,18 @@
 class Api::V1::ItemsController < ApplicationController
+
+  def create
+    item = Item.new(name: params[:name])
+    list = List.find_by(id: params[:listId])
+    list.items << item
+
+      render json: list.with_items
+  end
+
+  def destroy
+    byebug
+    item = Item.find_by(id: params[:id])
+    list = item.list
+    item.destroy
+      render json: list.with_items
+  end
 end
